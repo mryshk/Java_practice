9bc8aa42b230f94df70044319e8ac32a2a630113
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-16 11:13:41 +0900

    New project

---
+++ Mydiary.java
@@ -0,0 +1,7 @@
+public class Mydiary {
+    public static void main (String[] args) {
+        System.put.println("日記リスト");
+
+        System.put.prinln("終了します。");
+    }
+}
---
+++ Proj1/src/Int.java
@@ -0,0 +1,12 @@
+
+public class Int {
+   public static void main(String[] args) {
+	   int age = 25;
+	   System.out.println(age);
+	   
+	   int  a = 3;
+	   int variable2 = 5;
+	   int c = 3 * 5;
+	   System.out.println("縦幅3横幅5の長方形の面積は" + c);
+   }
+}
