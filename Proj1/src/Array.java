e6483d7a70eba5542b5766789b15238e4806fb83
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-17 23:22:22 +0900

    Add array

---
+++ Proj1/src/Array.java
@@ -0,0 +1,19 @@
+
+public class Array {
+	public static void main(String[] args) {
+		
+		int [] scores = new int[5];
+		
+		System.out.println(scores[3]);
+	
+		int num = scores.length;
+		System.out.println(num);
+		
+		int [] age = {23,45,6,34,25};
+		System.out.println(age[0]);
+		
+		String [] aiue = {"おk", "おk", "よしき","わかな"};
+		System.out.println( "むろ" + " " + aiue[2]);
+	}
+
+}
