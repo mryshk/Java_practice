7b0aeb0b34471203c93930ea233ca52e884f1852
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-18 17:23:11 +0900

    Add array

--- Proj1/src/Array.java
+++ Proj1/src/Array.java
@@ -14,6 +14,23 @@ public class Array {
 		
 		String [] aiue = {"おk", "おk", "よしき","わかな"};
 		System.out.println( "むろ" + " " + aiue[2]);
+		
+//		2次元配列
+		int [][] scores02 = new int [2][3];
+		
+		scores02[0][0] = 40;
+		scores02[0][1] = 50;
+		scores02[0][2] = 60;
+		
+		scores02[1][0] = 80;
+		scores02[1][1] = 70;
+		scores02[1][2] = 60;
+		
+		System.out.println(scores02[1][1]);
+		
+		int [][] age02 = {{40,30,40,50,50}, {20,34,55,5,6,7,4}};
+		System.out.println(age02[1][3]);
+		
 	}
 
 }
