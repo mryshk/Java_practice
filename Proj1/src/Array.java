213edc04b47982ff0d55424ffc4e3313ae7afd74
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-18 18:34:29 +0900

    Add array

--- Proj1/src/Array.java
+++ Proj1/src/Array.java
@@ -31,6 +31,34 @@ public class Array {
 		int [][] age02 = {{40,30,40,50,50}, {20,34,55,5,6,7,4}};
 		System.out.println(age02[1][3]);
 		
+		
+		int [] scores03 = {20,30,40,50,80};
+		int count = 0;
+		for (int i=0; i < scores03.length; i++) {
+			if (scores03[i] >= 50) {
+				count++;
+			}
+		}
+		System.out.println("50点以上の科目の数は" + count + "です。");
+		
+		int [] seq = new int[10];
+		
+		for (int i = 0; i < seq.length; i++) {
+			seq[i] = new java.util.Random().nextInt(4);
+		}
+		
+		for (int i=0; i<seq.length; i++) {
+			char [] base = {'A', 'T', 'G','C'};
+			System.out.println(base[seq[i]] + " ");
+		}
+		
+//		拡張for文
+		int [] scores04 = {30, 40, 50,20, 50,40,30};
+		for (int value : scores04) {
+			System.out.println(value);
+		}
+		
+
 	}
 
 }
