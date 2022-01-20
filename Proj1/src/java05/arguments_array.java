f25dd883a5c0b92568531acc591fbfd4eb4a4b23
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-20 22:58:15 +0900

    Learn arguments_array

---
+++ Proj1/src/java05/arguments_array.java
@@ -0,0 +1,61 @@
+package java05;
+
+public class arguments_array {
+//	public static void main(String[] args) {
+//		int[] array = {1,2,3};
+//		printArray(array);
+//	}
+//	
+//	public static void printArray(int[] ar) {
+//		
+//		for (int element: ar) {
+//			System.out.println(element);
+//		}
+//		
+//	}
+	
+//	配列の参照渡し。
+//	呼び出し先で加えた変更が呼び出し元にも影響するようになる。
+//	理由は、配列は値渡しではなく実在する配列を参照する形であるから。
+	
+//	public static void main(String[] args){
+//		int [] array = {1,2,3};
+//		incArray(array);
+//		
+//		for (int i : array) {
+//			System.out.println(i);
+//		}
+//		
+//	}
+//	
+//	public static void incArray(int [] array) {
+//		for (int i=0; i <array.length; i++) {
+//			array[i]++;
+////		}
+//	
+//		
+//
+//	}
+//	
+	
+//	戻り値に配列を用いる場合
+
+	public static void main(String[] args) {
+		int [] array = makeArray(5);
+		
+		for(int i : array) {
+			System.out.println(i);
+		}
+	}
+	
+	public static int [] makeArray(int size) {
+		int [] newArray = new int[size];
+		
+		for (int i=0; i < newArray.length; i++) {
+			newArray[i] = i;
+		}
+		return newArray;
+	}
+	
+	
+}
