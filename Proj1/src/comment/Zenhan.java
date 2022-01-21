fb18570f324368848a522d12339fef3215ec81a5
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-21 17:02:55 +0900

    do exersice

---
+++ Proj1/src/comment/Zenhan.java
@@ -0,0 +1,13 @@
+package comment;
+
+public class Zenhan {
+	
+    public static void doWarusa() {
+    	System.out.println("きなこでござる。食えませんがの。");
+    }
+    
+    public static void doTogame() {
+    	System.out.println("この老ぼれの目は誤魔化せんぞ");
+    }
+
+}
