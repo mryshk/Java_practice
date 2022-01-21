8e8140a07303583c3494a2f8faf261338028cc22
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-21 23:34:15 +0900

    do exersice

---
+++ Proj1/src/java08/Cleric.java
@@ -0,0 +1,33 @@
+package java08;
+
+import java.util.Random;
+
+public class Cleric {
+	String name;
+	int hp = 50;
+	final int MAX_HP = 50;
+	int mp = 10;
+	final int MAX_MP = 10;
+	
+	public void selfAid() {
+		this.mp -= 5;
+		this.hp = this.MAX_HP;
+		System.out.println("HPが全回復しました。");
+	}
+	public int pray(int sec) {
+		System.out.println(this.name + "は、" + sec + "秒間点に祈った");
+		
+		int recover = new Random().nextInt(3) + sec;
+		
+		int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+		
+		this.mp += recoverActual;
+		
+		System.out.println("MPが" + recoverActual + "回復した。");
+		
+		
+		System.out.println("MPが" + this.mp + "になりました。");
+		return recoverActual;
+	}
+
+}
