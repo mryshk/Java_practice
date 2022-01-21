727391112bb1587c9b7aaf4f7ef5f9ad709f13a9
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-21 15:25:50 +0900

    Learn package

---
+++ Proj1/src/calcapp/logic/CalcLogic.java
@@ -0,0 +1,17 @@
+package calcapp.logic;
+
+public class CalcLogic{
+
+  public static int tasu(int a, int b){
+         return a + b;
+         
+    }
+    public static int hiku(int a, int b){
+        return a - b;
+    }
+
+}
+
+
+  
+        
