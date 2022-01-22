59d67aafdb18e2ee1e0fa10679a54c755cb69132
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-22 16:19:37 +0900

    Learn class

---
+++ Proj1/src/java08/Wizard.java
@@ -0,0 +1,13 @@
+package java08;
+
+public class Wizard {
+	String name;
+	int hp;
+	
+	public void heal(Hero h) {
+		h.hp += 10;
+		System.out.println(h.name + "のHPを回復した。");
+		System.out.println(h.hp + "になった！");
+	}
+
+}
