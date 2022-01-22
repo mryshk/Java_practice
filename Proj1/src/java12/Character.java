a62ed410760808bc42073f076cc3282eb9166fd3
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-22 23:46:56 +0900

    Learn abstract class

---
+++ Proj1/src/java12/Character.java
@@ -0,0 +1,15 @@
+package java12;
+
+public abstract class Character {
+	String name;
+	int hp;
+	
+	public void run() {
+		System.out.println(this.name + "は逃げ出した。");
+	}
+	
+//	未詳細メソッド　≒ abstract 抽象メソッドという。
+	public abstract void attack(Matango m);
+		
+
+}
