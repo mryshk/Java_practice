d704d41ca56b0eac912cf58666f4498439e481bf
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-22 22:11:43 +0900

    do exersice

--- Proj1/src/java10/Main.java
+++ Proj1/src/java10/Main.java
@@ -3,9 +3,20 @@ package java10;
 public class Main {
 	public static void main(String[] args) {
 		Hero h = new Hero();
-		h.run();
-		SuperHero sh = new SuperHero();
-		sh.run();
+//		h.run();
+////		SuperHero sh = new SuperHero();
+////		sh.run();
+//		
+//		SuperHero sh = new SuperHero();
+//		
+//		Weapon w = new Weapon();
+//		System.out.println(w.name + w.price);
+		
+		PoisonMatango pm = new PoisonMatango('C');
+		
+		pm.attack(h);
+		System.out.println(h.hp);
+		System.out.println(pm.poisonCount);
 	}
 
 
