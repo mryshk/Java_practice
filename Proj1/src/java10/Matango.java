0ec54c8e44a7e8fe8ac128ab840a36befdebf804
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-22 22:11:29 +0900

    Learn super

--- Proj1/src/java10/Matango.java
+++ Proj1/src/java10/Matango.java
@@ -1,13 +1,25 @@
 package java10;
 
 public class Matango {
-	int hp = 50;
-	final int LEVEL = 10;
-	char suffix;
+//	int hp = 50;
+//	final int LEVEL = 10;
+//	char suffix;
+//	
+//	public void run() {
+//		System.out.println("おばけキノコは" + this.suffix + "逃げ出した。");
+//		hp -=100;
+//	}
 	
-	public void run() {
-		System.out.println("おばけキノコは" + this.suffix + "逃げ出した。");
-		hp -=100;
+	int hp = 50;
+	char suffix;
+	public Matango(char suffix) {
+		this.suffix = suffix;
+	}
+	
+	public void attack(Hero h) {
+		System.out.println("キノコ" + this.suffix + "の攻撃");
+		System.out.println("10のダメージ");
+		h.hp -= 10;
 	}
 	
 
