235cd36ec49ea8ef714b7096b4abf8d7d141e27d
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-24 22:13:51 +0900

    Learn extends

---
+++ Proj1/src/java11/Asset.java
@@ -0,0 +1,19 @@
+package java11;
+
+public abstract class Asset {
+	String name;
+	int price;
+	
+	public Asset(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public int getPreice() {
+		return this.price;
+	}
+
+}
