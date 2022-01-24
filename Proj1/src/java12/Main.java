c765806211e074a5947f2d84944f77830357cad0
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-24 22:36:21 +0900

    Learn polymorphism

---
+++ Proj1/src/java12/Main.java
@@ -0,0 +1,19 @@
+package java12;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Character c = new Wizard("yo");
+		
+		Matango m = new Matango("キノコ");
+		
+		c.attack(m);
+		
+
+		
+	}
+	
+
+	
+
+}
