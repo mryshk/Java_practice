00230bd16edab83a7ff179519025db5f16065ea8
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-24 22:14:22 +0900

    Learn extends

---
+++ Proj1/src/java11/IntangibleAsset.java
@@ -0,0 +1,15 @@
+package java11;
+
+public class IntangibleAsset extends Asset {
+	String patent;
+	
+	public IntangibleAsset(String name,int price, String patent) {
+		super(name,price);
+		this.patent = patent;
+	}
+	
+	
+
+	
+
+}
