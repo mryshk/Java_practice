2cf895b25cbe9701314e04138d9223a3113e52e8
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-24 22:14:14 +0900

    Learn extends

---
+++ Proj1/src/java11/Fool.java
@@ -0,0 +1,25 @@
+package java11;
+
+public class Fool extends Character implements Human  {
+	
+	public void attack(Matango m) {
+		System.out.println(this.name + "戦わずに遊んでいる。");	
+	}
+	
+	public void talk() {
+		
+	}
+	
+	public void hear() {
+		
+	}
+	
+	public void watch() {
+		
+	}
+	public void run() {
+		
+	}
+
+	
+}
