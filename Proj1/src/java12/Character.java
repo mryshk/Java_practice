a180db3976be40a04243e9e6a4c4e8c1cfb8bf29
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-24 22:35:56 +0900

    Learn polymorphism

---
+++ Proj1/src/java12/Character.java
@@ -0,0 +1,21 @@
+package java12;
+
+public abstract class Character {
+	String name;
+	int hp;
+	public Character(String name, int hp) {
+		this.name = name;
+		this.hp = hp;
+	}
+	
+	public Character(String name) {
+		this.name = name;
+		this.hp = 100;
+	}
+	public void run() {
+		System.out.println(this.name + "は逃げ出した。");
+	}
+	
+	public abstract void attack(Matango m);
+
+}
