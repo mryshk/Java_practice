3b6d1906177a5f7a32c2c5018797e9755a0e76a2
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-25 12:29:48 +0900

    Learn polumorphoism

--- Proj1/src/java12/Main.java
+++ Proj1/src/java12/Main.java
@@ -3,13 +3,39 @@ package java12;
 public class Main {
 	
 	public static void main(String[] args) {
-		Character c = new Wizard("yo");
+//		Character c = new Wizard("yo");
+//		
+//		Matango m = new Matango("キノコ");
+//		
+//		c.attack(m);
+//		
+		Slime s = new Slime("スライム1"); Monster mo = new Slime("スライム2");
+		Monster dragon = new Dragon("ドラゴン2");
+//		s.run();
+//		mo.run();
+//		dragon.run();
 		
-		Matango m = new Matango("キノコ");
+		Character[] c = new Character[5];
 		
-		c.attack(m);
-		
+		c[0] = new Hero("田中");
+		c[1] = new Hero("青木");
+		c[2] = new Thief("steven");
+		c[3] = new Wizard("yoshiki");
+		c[4] = new Wizard("sibuya");
 
+	
+		for (Character ch :c ) {
+			ch.run();
+		}
+		
+		Hero hero = new Hero("yeah");
+		Wizard wizard = new Wizard("魔法使い１");
+		
+		hero.attack(dragon);
+		wizard.attack(s);
+		
+		
+		
 		
 	}
 	
