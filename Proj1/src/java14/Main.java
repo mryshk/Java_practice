a0340610ddc9e4512de3e96897bb999c4a8ce179
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-25 20:50:35 +0900

    Learn static method

---
+++ Proj1/src/java14/Main.java
@@ -0,0 +1,28 @@
+package java14;
+
+public class Main {
+	public static void main(String[] args) {
+		Hero h1 = new Hero();
+		
+		h1.name = "ミナト";
+		h1.hp = 100;
+		Hero h2 = new Hero();
+		
+		h2.name = "ミナト";
+		h2.hp = 100;
+		
+		if (h1.equals(h2)) {
+			System.out.println("同じ内容です。");
+			
+		}else {
+			System.out.println("違う内容です。");
+		}
+		
+		Hero.setRandomMoney();
+		System.out.println(Hero.money);
+		
+		Hero h3 = new Hero();
+		System.out.println(h3.money);
+	}
+
+}
