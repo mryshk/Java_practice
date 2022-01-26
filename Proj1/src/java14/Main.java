60ffaaf992ba057026ceedbd24e05837a7cd7db3
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-26 15:10:23 +0900

    Learn method

--- Proj1/src/java14/Main.java
+++ Proj1/src/java14/Main.java
@@ -18,11 +18,20 @@ public class Main {
 			System.out.println("違う内容です。");
 		}
 		
+//		newせずともクラス型に対してメソッドを呼んでいる。＝　静的メソッド。
 		Hero.setRandomMoney();
 		System.out.println(Hero.money);
 		
 		Hero h3 = new Hero();
 		System.out.println(h3.money);
+		
+		Hero.money = 100;
+		System.out.println(Hero.money);
+		System.out.println(h2.money);
+		
+		h2.money = 300;
+		
+		System.out.println(h1.money);
 	}
 
 }
