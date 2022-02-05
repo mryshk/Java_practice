23ff31fffbe6ec5a00dc33fa94c63791191817d8
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-05 16:43:10 +0900

    Learn static

---
+++ Proj1/src/java16/Print.java
@@ -0,0 +1,13 @@
+package java16;
+
+import java.util.List;
+
+public class Print {
+	
+	public static void printList(List<String> list) {
+		for (String i : list) {
+			System.out.println(i);
+		}
+	}
+
+}
