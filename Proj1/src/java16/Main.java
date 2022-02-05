b98182adfe86df2c151feb18c2195eb9089b5b7e
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-05 16:43:02 +0900

    Learn list_set_map

--- Proj1/src/java16/Main.java
+++ Proj1/src/java16/Main.java
@@ -1,14 +1,35 @@
 package java16;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 	
 	
 	
 	public static void main(String[] args) {
+		
+//		ArrayListの取り出し方が３種類ある。
+//		通常for分と拡張for文、iteratorを使用する。
+		
+		ArrayList<Integer> grade  = new ArrayList<Integer>();
 		ArrayList<Integer> points = new ArrayList<Integer>();
+		
+		grade.add(56);
+		grade.add(45);
+		grade.add(89);
+		
+		Iterator<Integer> iterator = grade.iterator();
+		
+		while(iterator.hasNext()) {
+			Integer ok = iterator.next();
+			System.out.println(ok);
+		}
 		
 		ArrayList<String> names = new ArrayList<String>();
 		
@@ -30,10 +51,48 @@ public class Main {
 		points.add(34);
 		points.add(25);
 		
-		System.out.println(points.get(2));
+		System.out.println(points.get(0));
 		
 		for (int e : points) {
 			System.out.println(e);
+		}
+		
+		for (int i=0; i<points.size(); i++) {
+			System.out.println(points.get(i));
+		}
+		
+		Print.printList(names);
+		
+		Set<String> colors = new HashSet<String>();
+		colors.add("赤");
+		colors.add("青");
+		colors.add("緑");
+		
+		for (String s : colors) {
+			System.out.println(s + "→");
+		}
+		
+		Set<String> words = new TreeSet<String>();
+		
+		words.add("dogs");
+		words.add("animal");
+		words.add("cat");
+		
+		for (String w : words) {
+			System.out.print(w + "と");
+		}
+		
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		prefs.put("京都府",255);
+		prefs.put("東京都",1261);
+		prefs.put("熊本県", 181);
+		
+		int Tokyo = prefs.get("東京都");
+		System.out.println("東京都の人口は"+ Tokyo);
+		
+		for (String key : prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(key + "の人口は" + value);
 		}
 		
 		
